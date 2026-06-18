de777827d182cdca6f5f57cb471714d5cd27a614
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-19 01:00:34 +0200

    Add mobile profile endpoint url

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ReceiptViewSet, import_bank_statement, match_candidates, scan_receipt, summaries
+from .views import ReceiptViewSet, import_bank_statement, match_candidates, me, scan_receipt, summaries
 
 router = DefaultRouter()
 router.register('receipts', ReceiptViewSet, basename='receipts')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('me/', me),
     path('receipts/scan/', scan_receipt),
     path('bank/statement/', import_bank_statement),
     path('summaries/', summaries),
