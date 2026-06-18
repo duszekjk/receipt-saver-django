a734a091f5c8982b4b7d45de3c9a05acfee1b486
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-06-18 19:59:11 +0200

    Expose bank statement import from receipts urls

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import ReceiptViewSet, match_candidates, scan_receipt, summaries
+from .views import ReceiptViewSet, import_bank_statement, match_candidates, scan_receipt, summaries
 
 router = DefaultRouter()
 router.register('receipts', ReceiptViewSet, basename='receipts')
@@ -8,6 +8,7 @@ router.register('receipts', ReceiptViewSet, basename='receipts')
 urlpatterns = [
     path('', include(router.urls)),
     path('receipts/scan/', scan_receipt),
+    path('bank/statement/', import_bank_statement),
     path('summaries/', summaries),
     path('matches/review/', match_candidates),
 ]
