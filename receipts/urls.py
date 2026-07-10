6b18da3cb94a573623aae21c9c465011fbcd65c3
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-07-10 18:00:29 +0200

    Expose email and attachment import endpoint

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from . import bank_import_views, match_review_views
+from . import bank_import_views, email_import_views, match_review_views
 from .views import ReceiptViewSet, dashboard, dashboard_subcategory_details, match_candidates, me, scan_receipt, set_receipt_date, summaries
 
 router = DefaultRouter()
@@ -12,6 +12,7 @@ urlpatterns = [
     path('dashboard/subcategory/', dashboard_subcategory_details),
     path('receipts/scan/', scan_receipt),
     path('receipts/<int:receipt_id>/date/', set_receipt_date),
+    path('imports/email/', email_import_views.import_purchase_email),
     path('bank/statement/', bank_import_views.import_bank_statement),
     path('bank/import/', bank_import_views.import_bank_statement),
     path('bank/import/status/latest/', bank_import_views.latest_bank_import_status),
