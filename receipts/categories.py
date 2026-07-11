97b7a9b55e64234cf1a7e9935b17da9e8b935a6f
Jacek Kałużny <45168146+duszekjk@users.noreply.github.com>
2026-07-12 00:58:42 +0200

    Expand categories and add searchable synonyms

--- receipts/categories.py
+++ receipts/categories.py
@@ -1,5 +1,6 @@
 import unicodedata
 
+
 RECEIPT_CATEGORIES = {
     'Żywność': [
         'owoce', 'warzywa', 'pieczywo', 'nabiał', 'jaja', 'sery', 'jogurty', 'masło',
@@ -14,38 +15,65 @@ RECEIPT_CATEGORIES = {
     ],
     'Dom': [
         'chemia domowa', 'środki czystości', 'papier toaletowy', 'ręczniki papierowe',
-        'pranie', 'kuchnia', 'remont', 'narzędzia', 'ogród', 'kwiaty', 'dekoracje'
+        'pranie', 'kuchnia', 'wyposażenie domu', 'meble', 'remont', 'narzędzia',
+        'ogród', 'kwiaty', 'dekoracje'
     ],
     'Higiena': ['kosmetyki', 'higiena osobista', 'fryzjer'],
     'Transport': [
-        'paliwo', 'parking', 'komunikacja miejska', 'taxi', 'serwis samochodu',
-        'opłaty drogowe', 'autostrady', 'winiety'
+        'paliwo', 'parking', 'opłaty drogowe', 'komunikacja miejska', 'kolej',
+        'taxi', 'serwis samochodu', 'części samochodowe', 'myjnia'
     ],
     'Mieszkanie': ['prąd', 'gaz', 'woda', 'internet', 'telefon', 'czynsz', 'ogrzewanie', 'śmieci'],
     'Restauracje': ['restauracja', 'fast food', 'kawiarnia', 'cukiernia'],
     'Ubrania': ['odzież', 'obuwie', 'bielizna', 'naprawa ubrań'],
     'Akcesoria osobiste': ['parasole', 'portfele', 'okulary', 'zegarki', 'biżuteria', 'torby i plecaki'],
-    'Zwierzęta': ['karma', 'weterynarz', 'leki', 'akcesoria'],
-    'Hobby': ['książki', 'ogród', 'rękodzieło', 'sport', 'elektronika', 'prasa'],
-    'Rodzina': ['dzieci', 'wnuki', 'prezenty', 'uroczystości'],
+    'Zwierzęta': ['karma', 'weterynarz', 'leki', 'akcesoria', 'pielęgnacja'],
+    'Sport i rekreacja': ['sprzęt sportowy', 'siłownia', 'basen', 'zajęcia sportowe', 'rekreacja'],
+    'Kultura i media': ['książki', 'filmy', 'muzyka', 'gry', 'prasa'],
+    'Hobby': ['ogród', 'rękodzieło', 'kolekcjonerstwo', 'elektronika', 'modelarstwo'],
+    'Rodzina': ['dzieci', 'wnuki', 'opieka nad dziećmi', 'prezenty', 'uroczystości'],
     'Wydarzenia': ['pielgrzymki', 'rekolekcje', 'spotkania wspólnoty', 'konferencje', 'wyjazdy', 'wolontariat'],
-    'Finanse': ['bank', 'ubezpieczenie', 'podatki', 'opłata'],
+    'Finanse': ['bank', 'ubezpieczenie', 'podatki', 'opłata finansowa', 'kredyt i pożyczka'],
     'Darowizny': ['kościół', 'wspólnota', 'fundacja', 'rodzina'],
-    'Edukacja': ['studia', 'kursy', 'książki', 'szkolenia'],
-    'Subskrypcje': ['internetowe', 'streaming', 'oprogramowanie', 'aplikacje'],
+    'Edukacja': ['studia', 'kursy', 'materiały edukacyjne', 'szkolenia'],
+    'Subskrypcje': ['streaming wideo', 'streaming muzyki', 'chmura', 'oprogramowanie', 'aplikacje', 'prasa cyfrowa', 'AI'],
     'Hazard': ['lotto', 'zakłady sportowe', 'kasyno', 'poker', 'automaty', 'gry online'],
     'Alkohol': ['piwo', 'wino', 'mocny alkohol', 'likier', 'cydr', 'drinki'],
     'Opakowania i torby': ['reklamówki', 'torby papierowe', 'torby wielorazowe', 'opakowania', 'pojemniki'],
     'Opłaty techniczne': ['kaucja', 'depozyt', 'opłata serwisowa', 'opłata manipulacyjna', 'opłata dostawy'],
     'Promocje i korekty': ['rabat', 'kupon', 'zwrot', 'korekta ceny', 'zaokrąglenie'],
-    'Usługi': ['usługi sklepowe', 'naprawy', 'czyszczenie', 'drukowanie', 'dorabianie kluczy'],
+    'Usługi': ['usługi sklepowe', 'naprawy', 'czyszczenie', 'drukowanie', 'dorabianie kluczy', 'usługi profesjonalne'],
     'Biuro i papiernicze': ['papier', 'artykuły piśmiennicze', 'druk', 'koperty', 'organizacja dokumentów'],
-    'Elektronika i akcesoria': ['baterie', 'kable', 'ładowarki', 'akcesoria telefoniczne', 'akcesoria komputerowe'],
+    'Elektronika i akcesoria': ['baterie', 'kable', 'ładowarki', 'akcesoria telefoniczne', 'akcesoria komputerowe', 'sprzęt elektroniczny'],
     'Prezenty': ['upominki', 'kartki okolicznościowe', 'pakowanie prezentów'],
-    'Podróże': ['noclegi', 'bilety', 'bagaż', 'ubezpieczenie podróżne'],
-    'Administracyjne': ['urząd', 'poczta', 'mandaty', 'dokumenty'],
+    'Podróże': ['noclegi', 'bilety', 'bagaż', 'ubezpieczenie podróżne', 'wycieczki'],
+    'Administracyjne': ['urząd', 'poczta', 'mandaty', 'dokumenty', 'opłaty urzędowe'],
     'Nieczytelne pozycje': ['pozycja nieczytelna', 'skrót nierozpoznany', 'produkt niejednoznaczny'],
 }
+
+
+CATEGORY_SYNONYMS = {
+    ('Transport', 'opłaty drogowe'): [
+        'autostrada', 'autostrady', 'winieta', 'winiety', 'e-toll', 'etoll',
+        'bramka', 'bramki autostradowe', 'opłata za przejazd', 'droga płatna',
+        'płatny tunel', 'płatny most', 'amberone', 'autopay'
+    ],
+    ('Transport', 'parking'): ['postój', 'parkomat', 'mobilet', 'strefa parkowania'],
+    ('Transport', 'paliwo'): ['benzyna', 'diesel', 'olej napędowy', 'tankowanie', 'stacja paliw'],
+    ('Finanse', 'ubezpieczenie'): ['polisa', 'oc', 'ac', 'nnw', 'ubezpieczenia', 'składka ubezpieczeniowa'],
+    ('Kultura i media', 'książki'): ['książka', 'ebook', 'e-book', 'kindle', 'publio', 'legimi', 'audiobook'],
+    ('Kultura i media', 'filmy'): ['film', 'movie', 'dvd', 'blu-ray', 'bluray', 'vod', 'apple tv', 'wypożyczenie filmu'],
+    ('Kultura i media', 'muzyka'): ['album', 'płyta', 'cd', 'winyl', 'vinyl', 'itunes', 'utwór'],
+    ('Kultura i media', 'gry'): ['gra', 'steam', 'playstation', 'xbox', 'nintendo', 'dlc'],
+    ('Subskrypcje', 'streaming wideo'): ['netflix', 'max', 'disney+', 'prime video', 'abonament filmowy'],
+    ('Subskrypcje', 'streaming muzyki'): ['spotify', 'apple music', 'tidal', 'youtube music'],
+    ('Subskrypcje', 'chmura'): ['icloud', 'icloud+', 'google one', 'dropbox', 'onedrive', 'dysk w chmurze'],
+    ('Subskrypcje', 'AI'): ['chatgpt', 'openai', 'claude', 'gemini', 'sztuczna inteligencja'],
+    ('Dom', 'wyposażenie domu'): ['wyposażenie', 'artykuły domowe', 'gospodarstwo domowe'],
+    ('Sport i rekreacja', 'siłownia'): ['fitness', 'gym', 'karnet sportowy'],
+    ('Administracyjne', 'opłaty urzędowe'): ['opłata skarbowa', 'urząd', 'wniosek', 'administracja'],
+}
+
 
 BANK_TRANSACTION_CATEGORIES = RECEIPT_CATEGORIES | {
     'Przychody': ['wynagrodzenie', 'emerytura', 'renta', 'zwrot', 'darowizna otrzymana', 'sprzedaż', 'pozostałe przychody'],
@@ -116,3 +144,22 @@ def allowed_bank_categories_prompt_text():
         f'- {category}: {", ".join(subcategories)}'
         for category, subcategories in BANK_TRANSACTION_CATEGORIES.items()
     )
+
+
+def category_catalog_payload():
+    return {
+        'version': 2,
+        'categories': [
+            {
+                'name': category,
+                'subcategories': [
+                    {
+                        'name': subcategory,
+                        'synonyms': CATEGORY_SYNONYMS.get((category, subcategory), []),
+                    }
+                    for subcategory in subcategories
+                ],
+            }
+            for category, subcategories in RECEIPT_CATEGORIES.items()
+        ],
+    }
